5d48c7768204d186660e70ff6a8be142616f39e0
Jaydip Singh <jaydipsinghchouhan93@gmail.com>
2025-07-12 10:45:27 +0530

    home screen added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SignIn } from './auth/sign-in/sign-in';
+import { Homepage } from './homepage/homepage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: Homepage },
+  { path: '**', component: SignIn }
+];
